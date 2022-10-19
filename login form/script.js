ff31e7722fbd6eb2125d6f825c3918c7c46aa538
aniruddhabishwakarma <aniruddha.bishwakarma123@gmail.com>
2022-10-19 12:29:47 +0545

    login

---
+++ login form/script.js
@@ -0,0 +1,9 @@
+var modal = document.getElementById("modal");
+modal.style.display = "none";
+function myFunction() {
+if (modal.style.display !== "none") {
+    modal.style.display = "none";
+} else {
+    modal.style.display = "block";
+}
+}
